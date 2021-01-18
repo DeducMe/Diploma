0da51dc338d6d2acea93dd705ef1415d7d8946a1
DeducMe <deducMe@gmail.com>
2021-01-18 13:01:09 +0300

    true fix

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 8000;
+const PORT = 4000;
 
-app.use(express.static(path.join(__dirname,'build', 'index.html')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(PORT, ()=>{
     console.log('started')
 })
+
+app.get('*', (req, res) => {                       
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));                               
+});
