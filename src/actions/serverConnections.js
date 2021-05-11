7a9e627490d9bc6bc49087b130fe1bcd358b7fca
DeducMe <deducMe@gmail.com>
2021-05-11 16:50:31 +0300

    fix

--- src/actions/serverConnections.js
+++ src/actions/serverConnections.js
@@ -1,4 +1,4 @@
-const url = 'http://job-flow.ru/api'  //http://localhost:3000
+const url = 'https://job-flow.ru/api'  //http://localhost:3000
 function fetchError(){
     return{
         type:'FETCH_ERROR',
