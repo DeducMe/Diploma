aea728efa353d7d7a498c251c3b11b1f4b3bcd61
DeducMe <deducMe@gmail.com>
2021-05-12 21:38:36 +0300

    fix

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 const path = require('path');
 var cors = require('cors');
-const PORT = 4000;
-app.use(cors());
+
+var http = require('http');
+
+var server = http.createServer(app);
+
+app.set('port', (4000));
+
+app.use(function(req, res, next) {
+    var reqType = req.headers["x-forwarded-proto"];
+    reqType == 'https' ? next() : res.redirect("https://" + req.headers.host + req.url);
+});
 
 app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(PORT, ()=>{
+app.listen(4000, ()=>{
     console.log('started')
 })
 
-app.use(cors(), function (req, res, next) {
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Methods", "GET");
-    res.set("Access-Control-Allow-Headers", "Content-Type");
-    res.set("Access-Control-Max-Age", "3600");
-    res.header("Access-Control-Allow-Origin", "http://localhost:4000");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, PATCH, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "*, Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin, Access-Control-Allow-Origin");
-    next();
-  
-    // Pass to next layer of middleware
-});
-
-app.get('*',cors(), (req, res) => {  
-    res.header("Access-Control-Allow-Origin", "http://localhost:4000");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, PATCH, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "*, Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin, Access-Control-Allow-Origin");
-  
+app.get('*', cors(), (req, res) => {  
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));                               
 });
+
