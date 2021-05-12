284d9b5245a87622c13d157de55de115f295e63c
DeducMe <deducMe@gmail.com>
2021-05-12 21:58:26 +0300

    fix

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ var express = require('express');
 var app = express();
 const path = require('path');
 var cors = require('cors');
-
 var https = require('https');
+const fs = require('fs');
 
-var server = https.createServer(app);
+const port = 4000;
 
-app.set('port', (4000));
+var key = fs.readFileSync('./rootCA.key');
+var cert = fs.readFileSync('./rootCA.crt');
+var options = {
+  key: key,
+  cert: cert
+};
+
+var server = https.createServer(options, app);
+
+app.set('port', (port));
 
 app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(4000, ()=>{
+app.listen(port, ()=>{
     console.log('started')
 })
 
