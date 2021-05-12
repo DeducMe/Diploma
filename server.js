5b9a80679f2da00f72642834718bfbd5920d03eb
DeducMe <deducMe@gmail.com>
2021-05-12 22:26:45 +0300

    fix

--- server.js
+++ server.js
@@ -12,18 +12,38 @@ app.get('*', cors(), (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));                               
 });
 
+
+
 require("greenlock-express")
     .init({
         packageRoot: __dirname,
         configDir: "./greenlock.d",
- 
-        // contact for security and critical bug notices
+
         maintainerEmail: "jon@example.com",
- 
-        // whether or not to run at cloudscale
         cluster: false
     })
-    // Serves on 80 and 443
-    // Get's SSL certificates magically!
-    .serve(app);
+    .ready(httpsWorker);
 
+function httpsWorker(glx) {
+    //
+    // HTTPS 1.1 is the default
+    // (HTTP2 would be the default but... https://github.com/expressjs/express/issues/3388)
+    //
+    // Get the raw https server:
+    var httpsServer = glx.httpsServer(null, function(req, res) {
+        res.end("Hello, Encrypted World!");
+    });
+
+    httpsServer.listen(8443, "0.0.0.0", function() {
+        console.info("Listening on ", httpsServer.address());
+    });
+
+    // Note:
+    // You must ALSO listen on port 80 for ACME HTTP-01 Challenges
+    // (the ACME and http->https middleware are loaded by glx.httpServer)
+    var httpServer = glx.httpServer();
+
+    httpServer.listen(8080, "0.0.0.0", function() {
+        console.info("Listening on ", httpServer.address());
+    });
+}
