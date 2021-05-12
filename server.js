230621db433bf359f6398d10e54d5612bf40ae93
DeducMe <deducMe@gmail.com>
2021-05-12 22:18:14 +0300

    fix

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const path = require('path');
 var cors = require('cors');
 var http = require('http');
 var https = require('https');
-const fs = require('fs');
-const LEX = require('greenlock-express');
 
 app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'build')));
@@ -14,16 +12,18 @@ app.get('*', cors(), (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));                               
 });
 
-const lex = LEX.create({
-    server: 'staging',
-    email: 'my.email@mailprovider.com',
-    agreeTos: true,
-    configDir: 'cert/',
-    approveDomains: ['job-flow.ru']
-});
+require("greenlock-express")
+    .init({
+        packageRoot: __dirname,
+        configDir: "./greenlock.d",
+ 
+        // contact for security and critical bug notices
+        maintainerEmail: "jon@example.com",
+ 
+        // whether or not to run at cloudscale
+        cluster: false
+    })
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    .serve(app);
 
-var httpServer = http.createServer(lex.middleware(require('redirect-https')()));
-var httpsServer = https.createServer(lex.httpsOptions, lex.middleware(app));
-
-httpServer.listen(8080);
-httpsServer.listen(8443);
